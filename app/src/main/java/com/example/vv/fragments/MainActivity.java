b10bdd6a698a35055642e1d97fcabe225428ebb9
vv <u0NEEp9v>
2018-01-29 19:14:22 +0300

    Two Fragments

--- app/src/main/java/com/example/vv/fragments/MainActivity.java
+++ app/src/main/java/com/example/vv/fragments/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.vv.fragments;
 
+import android.content.res.Configuration;
+import android.graphics.drawable.GradientDrawable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity{
 
+    Fragment titles = new TitleFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (getResources().getConfiguration().orientation ==
+                Configuration.ORIENTATION_LANDSCAPE){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.left, titles)
+                    .add(R.id.right, new WebFragment())
+                    .commit();
+        }
+        else {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.main, titles)
+                    .commit();
+        }
     }
 }
 
